b7da81696d39b340faf54c587776b30590facc50
ngrajles1 <15281436+ngrajales1@users.noreply.github.com>
2019-03-01 22:46:04 -0600

    Forgot paren on Sequential. Finished loss function term

--- Neural Networks/IMDB Data in Keras/IMDB_In_Keras.py
+++ Neural Networks/IMDB Data in Keras/IMDB_In_Keras.py
@@ -78,7 +78,7 @@ print(y_test.shape)
 
 # TODO: Build the model architecture
 #creating the sequential model
-IMBD_Model = Sequential
+IMBD_Model = Sequential()
 
 #First Layer
 IMBD_Model.add(Dense(128, activation='relu', input_dim=1000))
@@ -88,7 +88,7 @@ IMBD_Model.add(Dense(64, activation='sigmoid'))
 IMBD_Model.add(Dense(2, activation='softmax'))
 
 # TODO: Compile the model using a loss function and an optimizer.
-IMBD_Model.compile(loss="categorical_cross", optimizer="adam", metrics=["accuracy"])
+IMBD_Model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 IMBD_Model.summary()
 
 # ## 5. Training the model
